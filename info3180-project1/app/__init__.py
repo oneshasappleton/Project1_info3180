882d8cf830e5d13a2010adb9ad317b48c6d30cee
oneshasappleton <44047570+oneshasappleton@users.noreply.github.com>
2020-04-30 03:33:06 -0500

    Update __init__.py

--- info3180-project1/app/__init__.py
+++ info3180-project1/app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "change this to be a more random key"
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://info3180-user:unlock@localhost/info3180-proj1" - local database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://liksbqgocctrmk:4d1659399cf74a6fc2e0ae82bce40fde99db170ce1e2af30824df56de3a63ef4@ec2-50-17-227-28.compute-1.amazonaws.com:5432/ddqjam3m7ipl4n'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
-app.config['UPLOAD_FOLDER'] = './app/static/uploads'
+
+
+#SQLALCHEMY_DATABASE_URI = "postgresql://bread:test@localhost/project1"
+SQLALCHEMY_DATABASE_URI= 'postgresql://liksbqgocctrmk:4d1659399cf74a6fc2e0ae82bce40fde99db170ce1e2af30824df56de3a63ef4@ec2-50-17-227-28.compute-1.amazonaws.com:5432/ddqjam3m7ipl4n'
+SQLALCHEMY_TRACK_MODIFICATIONS = False # added just to suppress a warning
+UPLOAD_FOLDER = "./app/static/uploads"
+SECRET_KEY= "MYKEY"
 
 db = SQLAlchemy(app)
 
