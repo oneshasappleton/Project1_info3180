f5ac5ebb616ad014af0aa63509ae41dd2c0adcc6
onesha sappleton <Sappleton101geo@gmail.com>
2020-05-13 20:39:48 -0500

    Final Submission

--- info3180-project1/app/__init__.py
+++ info3180-project1/app/__init__.py
@@ -6,7 +6,7 @@ UPLOAD_FOLDER = "./app/static/uploads"
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:nesha032898@localhost/people"
-#SQLALCHEMY_DATABASE_URI= 'postgresql://liksbqgocctrmk:4d1659399cf74a6fc2e0ae82bce40fde99db170ce1e2af30824df56de3a63ef4@ec2-50-17-227-28.compute-1.amazonaws.com:5432/ddqjam3m7ipl4n'
+#app.config['SQLALCHEMY_DATABASE_URI']= "postgresql://oxfumqjlrqlycs:45a5b5a9fbd0f753ef38400a2ab8f27db48e5d996e4cbc7a72c29e2a0c8d3f5f@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d9l9qglanjif6p"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['SECRET_KEY']= "MYKEY"
 db = SQLAlchemy(app)
